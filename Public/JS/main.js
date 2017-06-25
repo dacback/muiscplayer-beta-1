b5c69eda4d4c512185989815bc8e88b52a0ccf1d
Chris Barriteau <chris4finance@gmail.com>
2017-06-25 11:45:32 -0400

    Updated working version of main.js. Removed OOP syntax

--- Public/JS/main.js
+++ Public/JS/main.js
@@ -1,143 +1,58 @@
+var renderSong = function(songContent) {
 
-function Songs(song, artist, album, genre){
-	this.song = song;
-	this.artist = artist;
-	this.album = album;
-	this.genre = genre;
-	var musicGenre;
-	var pList;
-	var songId = new Date().getTime();
+    var songEntry = '<div class="pList">';
+    songEntry += '<audio class="songPlay>'
+    songEntry += '<p class="list-song">Song: ' + songContent.name + '</p>';
+    songEntry += '<p class="list-artist">Artist: ' + songContent.artist + '</p>';
+    songEntry += '<p class="list-album">Album: ' + songContent.album + '</p>';
+    songEntry += '<p class="list-genre">Genre: ' + songContent.genre + '</p>';
+    songEntry += '</audio>'
+    songEntry += '</div>';
 
-	this.getId = function(){
-		return songId
-	}
+    return songEntry;
 
-	this.songInfo = function(){
-		return this.song + " :" + this.artist;
-	}
-
-
-	// this.getSong = function(){
-	// 	return document.getElementById(this.tune).src;
-	// };
-
-	// this.setAudio = function(){
-	// 	var audio = document.getElementsByTagName(audioTag);
-	// 	audio.preload = 'auto';	
-	// 	return audio;	
-	// }
-
-	// this.playCurrentSong = function(audioTag){
-	// 	var audio = document.getElementsByTagName(audioTag)[0];
-	// 	audio.preload = 'auto';	
-	// 	audio.play();
-	// }
-}
-// var PlaySong = new Songs(1, 'song-play');
-
-Songs.prototype.createMusicGenre = function(){
-	musicGenre = this.createGenreObj();
-	musicGenre.songId = this.getId();
-	musicGenre.song = this.song;
-	musicGenre.artist = this.artist;
-	musicGenre.album = this.album;
-	musicGenre.genre = this.genre;
-		return musicGenre;
 };
 
-Songs.prototype.createPlayList = function(){
-	pList.push(this.createMusicGenre());
-		return pList;
+function renderPlayList(songs) {
+
+    var playList = document.getElementById("play-listing");
+    var songContentEntry = " ";
+
+    for (var prop in songs) {
+
+        songContentEntry = renderSong(songs[prop]);
+        playList.innerHTML += songContentEntry;
+
+    }
+
 };
 
 
-function Music(){
-	Songs.apply(this, arguments);
-}
+document.addEventListener("DOMContentLoaded", function() {
 
-Music.prototype = new Songs();
+    var song = document.getElementById('song-play');
 
-Music.prototype.createMusicGenre = function(){
-	musicGenre = this.createGenreObj();
-	musicGenre.songId = this.getId();
-	musicGenre.song = this.song;
-	musicGenre.artist = this.artist;
-	musicGenre.album = this.album;
-	musicGenre.genre = this.genre;
-		return musicGenre;
-};
+    function playSong() {
 
+        song.play();
+    }
 
-Music.prototype.createPlayList = function(){
-	pList = this.createPlistArr();
-	pList.push(this.createMusicGenre());
-		return pList;
-};
+    function pauseSong() {
+        song.pause();
+    }
 
+    fetch('/songs').then(function(response) {
 
-// ---------------- test run of Music conctructor
+        return response.json();
 
-var rockMusic = new Music("Scar Tissue");
-rockMusic.artist = "RHCP";
-rockMusic.album = "Californication";
-rockMusic.genre = "Alternative Rock"
+    }).then(function(data) {
 
+        renderPlayList(data.songs);
 
+    })
 
+    var songToPlay = document.getElementById('track-1');
+    songToPlay.addEventListener('click', playSong);
+    songToPlay.addEventListener('dblclick', pauseSong);
 
-
-
-
-// ---------------------------------------------------------------------
-document.addEventListener("DOMContentLoaded", function(){
-	console.log('hello world');
-
-	var song = document.getElementById('song-play');
-		console.log(song);
-
-	function playSong(){
-		// debugger;
-		song.play();
-	}
-
-	function pauseSong(){
-		song.pause();
-	}
-
-		var songToPlay = document.getElementById('track-1');
-		songToPlay.addEventListener('click', playSong);
-
-		songToPlay.addEventListener('dblclick', pauseSong);
-
-})
-
-	// PlaySong.playCurrentSong(x);
-
-	// var audio = document.getElementsByTagName('audio')
-	// song = CurrentSong.getSong();
-	// var play = CurrentSong.playCurrentSong();
-
-		// song = audio.src; 
-		// console.log(song);
-	// var sound = new Audio(song);
-	// console.log(sound + "I am NOT hearing you");
-
-	// function playSong(song){
-		// var elem = document.getElementById('current-song');
-		// var audio = document.createElement('audio');
-		// elem.appendChild(audio);
-		// var audio = document.getElementsByTagName('audio');
-		// audio.preload = 'auto';
-
-		// audio.src = song;
-		// console.log(play);
-		// 	play();
-			// console.log("Can you hear me!!!!!")
-	// }
-
-	// var songToPlay = document.getElementById('track-1');
-	// 	songToPlay.addEventListener('click',playSong);
-
-
-
-
+});
--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var client_id = '994d67ba07eb4acca5713c4140410ae8'; // Your client id
-var client_secret = '267d460733b34dd8a8b3a473a671536b'; // Your secret
-var redirect_uri = 'http://localhost:3050'; // Your redirect uri
-
 var express = require("express");
 var path = require("path");
 var app = express();
+var db = require('./Database');
+// console.log(db);
 var port = 3050;
 
 var publicPath = path.resolve(__dirname, "public");
@@ -12,6 +10,11 @@ app.use(express.static(publicPath));
 
 app.get('/', function(req,res){
 	res.sendFile(__dirname +'/public');
+});
+
+app.get('/songs',function(req,res){
+	console.log(db);
+	res.json(db);
 });
 
 app.listen(port,function(){
